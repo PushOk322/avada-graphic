d10a509e0b742103a9a746db643ae166fd158224
Paul Tyshkovets <tishkovets.pavlo@gmail.com>
2025-06-25 20:35:58 +0300

    scrolling animation finish

--- src/scroll.js
+++ src/scroll.js
@@ -2,53 +2,82 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.timeline({
+const tl = gsap.timeline({
 	scrollTrigger: {
-		trigger: ".scene-1",
-		scrub: true,
+		trigger: ".scene",
+		start: "top top",
+		end: "3000px",
+		scrub: 1,
 		pin: true,
 		markers: true,
 	},
+});
+tl.to(".phone-1", {
+	rotate: 15,
+	x: 700,
+	rotateY: 30,
+	scale: 1.1,
+	duration: 0.7,
+	ease: "power2.out",
 })
-	.to(".phone-1", {
-		rotate: 15,
-		x: 700,
-		duration: 0.875,
-	})
 	.to(".phone-1", {
 		rotate: 0,
 		x: 800,
-		duration: 0.125,
-	});
+		rotateY: 0,
+		scale: 1,
+		duration: 0.1,
+		ease: "power2.inOut",
+	})
 
-gsap.timeline({
-	scrollTrigger: {
-		trigger: ".scene-2",
-		scrub: true,
-		pin: true,
-		markers: true,
-	},
-}).to(".phone-2", {
-	scale: 0.5,
-	opacity: 0,
-	y: 200,
-});
-
-gsap.timeline({
-	scrollTrigger: {
-		trigger: ".scene-3",
-		scrub: true,
-		pin: true,
-		markers: true,
-	},
-}).fromTo(
-	".phone-3",
-	{
-		opacity: 0,
-		rotate: -45,
-	},
-	{
-		opacity: 1,
+	.set(".phone-2", {
+		visibility: "visible",
+		x: 800,
 		rotate: 0,
-	}
-);
+		rotateY: 0,
+		scale: 0.9,
+	})
+	.set(".phone-1", { visibility: "hidden" })
+	.to(".phone-2", {
+		rotateY: -180,
+		x: 400,
+		scale: 1.05,
+		duration: 0.7,
+		ease: "power2.out",
+	})
+	.to(".phone-2", {
+		rotateY: 0,
+		x: 0,
+		scale: 1,
+		duration: 0.5,
+		ease: "power2.inOut",
+	})
+
+	.set(".phone-3", {
+		visibility: "visible",
+		x: 0,
+		rotate: 0,
+		scale: 0.8,
+		opacity: 0,
+	})
+	.set(".phone-2", { visibility: "hidden" })
+	.to(".phone-3", {
+		opacity: 1,
+		rotate: -360,
+		scale: 1.1,
+		duration: 0.5,
+		ease: "back.out(1.7)",
+	})
+	.to(".phone-3", {
+		x: 800,
+		rotate: 0,
+		scale: 1,
+		duration: 0.7,
+		ease: "power2.inOut",
+	})
+	.to(".phone-3", {
+		x: 400,
+		rotate: 0,
+		scale: 1,
+		duration: 0.7,
+		ease: "power2.inOut",
+	});
