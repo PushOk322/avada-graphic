bf9133cc7331fe5a079d10cc4697913f5f33fe6b
Paul Tyshkovets <tishkovets.pavlo@gmail.com>
2025-06-28 21:56:00 +0300

    lines start

--- src/canvas.js
+++ src/canvas.js
@@ -4,8 +4,12 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.globalCompositeOperation = "destination-out";
+ctx.font = "bold 100px Arial";
+ctx.textAlign = "center";
+ctx.fillStyle = "white";
+ctx.fillText("COSMO", 843, 218);
 
+ctx.globalCompositeOperation = "destination-out";
 ctx.font = "bold 100px Arial";
 ctx.textAlign = "center";
 ctx.fillText("COSMO", 845, 220);
@@ -14,14 +18,20 @@ const planetaryCoordinatesX = 320;
 const planetaryCoordinatesY = 350;
 
 ctx.beginPath();
-ctx.arc(planetaryCoordinatesX, planetaryCoordinatesY, 60, 0, 360);
+ctx.arc(planetaryCoordinatesX, planetaryCoordinatesY, 60, 0, Math.PI * 2);
 ctx.fill();
 
 const orbits = [160, 210, 260];
 ctx.lineWidth = 3;
 orbits.forEach((radius) => {
 	ctx.beginPath();
-	ctx.arc(planetaryCoordinatesX, planetaryCoordinatesY, radius, 0, 360);
+	ctx.arc(
+		planetaryCoordinatesX,
+		planetaryCoordinatesY,
+		radius,
+		0,
+		Math.PI * 2
+	);
 	ctx.stroke();
 });
 
@@ -33,15 +43,55 @@ orbits.forEach((radius, i) => {
 	ctx.globalCompositeOperation = "source-over";
 
 	ctx.beginPath();
-	ctx.arc(x, y, 30, 0, 360);
+	ctx.arc(x, y, 30, 0, Math.PI * 2);
 	ctx.fillStyle = "black";
 	ctx.fill();
 
 	ctx.globalCompositeOperation = "destination-out";
 
 	ctx.beginPath();
-	ctx.arc(x, y, 20, 0, 360);
+	ctx.arc(x, y, 20, 0, Math.PI * 2);
 	ctx.fill();
 });
 
 ctx.globalCompositeOperation = "source-over";
+
+ctx.beginPath();
+ctx.arc(
+	planetaryCoordinatesX, // center x
+	planetaryCoordinatesY, // center y
+	350, // radius
+	Math.PI, // start angle (180°, left)
+	2 * Math.PI // end angle (360°, right)
+);
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.stroke();
+
+// Draw a quadratic Bezier curve
+ctx.beginPath();
+ctx.moveTo(200, 700); // Start point
+ctx.quadraticCurveTo(
+	600,
+	200, // Control point (pulls the curve)
+	1000,
+	700 // End point
+);
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.stroke();
+
+// Draw a cubic Bezier curve
+ctx.beginPath();
+ctx.moveTo(200, 800); // Start point
+ctx.bezierCurveTo(
+	400,
+	400, // First control point
+	800,
+	1200, // Second control point
+	1000,
+	800 // End point
+);
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.stroke();
