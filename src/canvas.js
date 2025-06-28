c88858eebf8cc862f5810991fd82a9e257ad169f
Paul Tyshkovets <tishkovets.pavlo@gmail.com>
2025-06-28 21:33:24 +0300

    trygonometria blyat

--- src/canvas.js
+++ src/canvas.js
@@ -10,30 +10,37 @@ ctx.font = "bold 100px Arial";
 ctx.textAlign = "center";
 ctx.fillText("COSMO", 845, 220);
 
+const planetaryCoordinatesX = 320;
+const planetaryCoordinatesY = 350;
+
+ctx.beginPath();
+ctx.arc(planetaryCoordinatesX, planetaryCoordinatesY, 60, 0, 360);
+ctx.fill();
+
 const orbits = [160, 210, 260];
-ctx.lineWidth = 2;
+ctx.lineWidth = 3;
 orbits.forEach((radius) => {
 	ctx.beginPath();
-	ctx.arc(320, 350, radius, 0, Math.PI * 2);
+	ctx.arc(planetaryCoordinatesX, planetaryCoordinatesY, radius, 0, 360);
 	ctx.stroke();
 });
 
 orbits.forEach((radius, i) => {
-	const angle = (Math.PI / 1.5) * (i + 1);
-	const x = 320 + radius * Math.cos(angle);
-	const y = 350 + radius * Math.sin(angle);
+	const angle = 180 * (i + 1);
+	const x = planetaryCoordinatesX + radius * Math.cos(angle);
+	const y = planetaryCoordinatesY + radius * Math.sin(angle);
 
 	ctx.globalCompositeOperation = "source-over";
 
 	ctx.beginPath();
-	ctx.arc(x, y, 30, 0, Math.PI * 2);
+	ctx.arc(x, y, 30, 0, 360);
 	ctx.fillStyle = "black";
 	ctx.fill();
 
 	ctx.globalCompositeOperation = "destination-out";
 
 	ctx.beginPath();
-	ctx.arc(x, y, 20, 0, Math.PI * 2);
+	ctx.arc(x, y, 20, 0, 360);
 	ctx.fill();
 });
 
